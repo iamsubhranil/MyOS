f89d3a99ac3f95801c275147f9f823fd2d02c5b6
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-04 21:10:38 +0530

    new: added memcmp, memcpy, memmove and memset

--- string.h
+++ string.h
@@ -2,5 +2,10 @@
 
 #include "myos.h"
 
-siz  strlen(const char *str);
-void memset(void *mem, u8 value, u32 size);
+extern "C" {
+siz   strlen(const char *str);
+void  memset(void *mem, u8 value, siz size);
+void *memcpy(void *dest, const void *source, siz size);
+void *memmove(void *dest, const void *source, siz size);
+int   memcmp(const void *s1, const void *s2, siz size);
+}
