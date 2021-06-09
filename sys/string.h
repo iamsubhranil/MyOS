b5c19d02931d29ee406e61cd81a5f16ea688384b
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-06-09 11:11:06 +0530

    fix: memset: use different memset for different word sizes

--- sys/string.h
+++ sys/string.h
@@ -5,8 +5,12 @@
 extern "C" {
 extern siz strlen(const char *str);
 // arch/x86/memset.S
-// memset automatically copies in 4byte chunks if required
+// memset automatically copies in 4byte chunks if required.
+// size is always in bytes, and must be prealigned
+// for w and l, that is not checked.
 extern void memset(void *mem, u8 value, siz size);
+extern void memsetw(void *mem, u16 value, siz size);
+extern void memsetl(void *mem, u32 value, siz size);
 // arch/x86/memcpy.S
 // copies in 4byte chunks if required
 extern void *memcpy(void *dest, const void *source, siz size);
